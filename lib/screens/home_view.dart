c09c506d4cf33651c5783504cb7890468ae1b521
Kevin Morainville <slupshi@gmail.com>
2023-12-14 14:36:31 +0100

    toto

--- lib/screens/home_view.dart
+++ lib/screens/home_view.dart
@@ -12,7 +12,7 @@ class HomeView extends StatelessWidget {
       hasLeadingAvatar: true,
       body: Center(
         child: TextButton(
-          child: const Text("Home to detail route button v1.2 bis bis"),
+          child: const Text("Home to detail route button v1.2 bis bis tag"),
           onPressed: () => const HomeDetailViewRouteData(id: 456).go(context),
         ),
       ),
