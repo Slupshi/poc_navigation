f0b8d190d9ca94c496b43a46b9433e83a6c489f1
Kevin Morainville <slupshi@gmail.com>
2023-12-14 15:16:44 +0100

    Push release 2.2 num 1

--- lib/screens/home_view.dart
+++ lib/screens/home_view.dart
@@ -12,7 +12,7 @@ class HomeView extends StatelessWidget {
       hasLeadingAvatar: true,
       body: Center(
         child: TextButton(
-          child: const Text("Home to detail route button feature 2"),
+          child: const Text("Home to detail route button release 2.2"),
           onPressed: () => const HomeDetailViewRouteData(id: 456).go(context),
         ),
       ),
